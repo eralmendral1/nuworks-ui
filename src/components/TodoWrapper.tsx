53fa678bbc606b5043bb8bbdc7d6b8037272e80e
m1racl3 <eralmendral@gmail.com>
2023-10-29 17:57:51 +0800

    cleanup

--- src/components/TodoWrapper.tsx
+++ src/components/TodoWrapper.tsx
@@ -7,7 +7,6 @@ export const TodoWrapper: React.FC = () => (
         <div className="mx-auto max-w-[720px] mt-12">
             <h1 className='font-bold text-3xl text-gray-600'>Nuworks Todos</h1>
             <TodoForm />
-            {/* todo: Record here, show completed count, and uncompleted count */}
             <TodoList />
         </div>
     </>
