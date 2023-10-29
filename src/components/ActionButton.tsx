b83066b61f0a8eda1a6fb83f724585a58cf89db6
m1racl3 <eralmendral@gmail.com>
2023-10-29 17:26:38 +0800

    fix: react import not used

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function ActionButton(props: any) {
-  return (
-    <button {...props} className="mx-2 flex items-center bg-gray-100 transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-gray-300 text-gray-600 pl-3 pr-6 py-2 text-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-600">
-        {props.children}
-    </button>
-  )
+const ActionButton: React.FC<any> = (props: any) => {
+    return (
+        <button {...props} className="mx-2 flex items-center bg-gray-100 transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-gray-300 text-gray-600 pl-3 pr-6 py-2 text-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-600">
+            {props.children}
+        </button>
+    )
 }
 
 export default ActionButton
