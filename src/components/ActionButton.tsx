1b61ee4d60129abda391da609c4fcfb4b85662ca
m1racl3 <eralmendral@gmail.com>
2023-10-29 18:03:44 +0800

    fix: duplicate classes

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ActionButton: React.FC<any> = (props: any) => {
     return (
-        <button {...props} className="mx-2 flex items-center bg-gray-100 transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-gray-300 text-gray-600 pl-3 pr-6 py-2 text-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-600">
+        <button {...props} className="mx-2 flex items-center bg-gray-100 transition duration-150 ease-in-out rounded border border-gray-300 text-gray-600 pl-3 pr-6 py-2 text-sm hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-600">
             {props.children}
         </button>
     )
